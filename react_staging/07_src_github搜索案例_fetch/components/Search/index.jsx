8702ebdf1c2177852ce67670b32263d244fbfc39
ivy <840464844@qq.com>
2021-02-26 16:12:40 +0800

    github fetch实现搜索案例

--- react_staging/07_src_github搜索案例_fetch/components/Search/index.jsx
+++ react_staging/07_src_github搜索案例_fetch/components/Search/index.jsx
@@ -22,17 +22,31 @@ export default class Search extends Component {
 		) */
 		//#endregion
 			
+		/*关于Promise的知识
+		 当位置1 返回的是一个非promise值，那么位置0得到的就是一个成功的Promise，且promise对象中的值就是位置1返回的值，状态是成功的
+		 当位置1 返回的是一个promise值，那么位置0得到的promise就是位置0返回的promise对象 状态是成功的
+		 当位置1 返回的是一个pending状态的promise，那么位置0得到的promise实例也是一个pending状态的 状态是成功的
+		 当位置1 中抛出了异常 那么位置0得到的promise实例状态为失败的 失败的原因就是你抛出的异常
+
+		 位置2 失败的返回值为undefined 他是一个非promise值，所以在位置0会得到成功的promise对象，且值为undefined
+				所以此时还会去调用下一个then方法中的成功的方法
+
+		解决方法：在位置4 加一句 return new Promise(()=>{}) 也就是返回了一个初始化状态的promise实例，
+				这样他就不会再去执行第二个then了
+
+		
+		*/
 		//发送网络请求---使用fetch发送（未优化）
-		/* fetch(`/api1/search/users2?q=${keyWord}`).then(
-			response => {
+		/* fetch(`/api1/search/users2?q=${keyWord}`).位置0 then(
+			  response => {
 				console.log('联系服务器成功了');
-				return response.json()
+				位置1 return response.json()
 			},
-			error => {
-				console.log('联系服务器失败了',error);
-				return new Promise(()=>{})
+			 error => {
+				位置2 console.log('联系服务器失败了',error);
+				位置4 // return new Promise(()=>{})
 			}
-		).then(
+		).then(位置3
 			response => {console.log('获取数据成功了',response);},
 			error => {console.log('获取数据失败了',error);}
 		) */
